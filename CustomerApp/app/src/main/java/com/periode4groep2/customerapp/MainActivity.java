1ddd668d8a6842a1b410535d08ae1b44aa2252c1
timsteijn <tjbsteij@avans.nl>
2017-05-02 13:17:21 +0200

    Jordan weet niks over gorden

--- CustomerApp/app/src/main/java/com/periode4groep2/customerapp/MainActivity.java
+++ CustomerApp/app/src/main/java/com/periode4groep2/customerapp/MainActivity.java
@@ -11,5 +11,4 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 }
-//geen test a neef
-//fresh
+//Ik hoef niet te tanken schat oke? DAAAG
