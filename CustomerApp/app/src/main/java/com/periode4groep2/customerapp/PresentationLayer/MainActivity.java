dd89232b46bc3e984f92ed28e7bee811aaceb1db
jesse <jwit11@avans.nl>
2017-05-04 11:11:04 +0200

    Toolbar werkend gekregen

--- CustomerApp/app/src/main/java/com/periode4groep2/customerapp/PresentationLayer/MainActivity.java
+++ CustomerApp/app/src/main/java/com/periode4groep2/customerapp/PresentationLayer/MainActivity.java
@@ -1,8 +1,9 @@
 package com.periode4groep2.customerapp.PresentationLayer;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
@@ -10,6 +11,8 @@ import com.periode4groep2.customerapp.R;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button loginButton;
+    private Toolbar toolbar;
+    private Button balanceButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,12 +20,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         loginButton = (Button)findViewById(R.id.inlogKnopId);
         loginButton.setOnClickListener(this);
+
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+
+        balanceButton = (Button) findViewById(R.id.buttonBalance);
+        balanceButton.setOnClickListener(this);
     }
 
     //Deze button moet nog veranderd worden wanneer de gegevens kloppen etc
     @Override
     public void onClick(View v) {
-        Intent intent = new Intent(this, HomeScreen.class);
-        startActivity(intent);
+        if(v.equals(loginButton)) {
+            Intent intent = new Intent(this, HomeScreen.class);
+            startActivity(intent);
+        } else if (v.equals(balanceButton)){
+            Intent intent = new Intent(this, add_saldo.class);
+            startActivity(intent);
+        }
+
     }
+
 }
