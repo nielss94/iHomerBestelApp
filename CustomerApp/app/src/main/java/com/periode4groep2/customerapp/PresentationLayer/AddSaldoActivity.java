bda948b207de865855297bae848661b72d89363d
rickvoermans <styledbyricky@gmail.com>
2017-05-04 23:09:23 +0200

    Shit keeps bugging + too late

--- CustomerApp/app/src/main/java/com/periode4groep2/customerapp/PresentationLayer/AddSaldoActivity.java
+++ CustomerApp/app/src/main/java/com/periode4groep2/customerapp/PresentationLayer/AddSaldoActivity.java
@@ -29,6 +29,7 @@ public class AddSaldoActivity extends AppCompatActivity implements View.OnClickL
     @Override
     public void onClick(View v) {
         balance = (Button)findViewById(R.id.buttonBalance);
+        TextView saldo = (TextView)findViewById(R.id.huidigSaldoTextMoney);
         //Hier laat ik een toast weergeven als de gebruiker op de button heeft geklikt
         //Hij pakt binnen de onCLick de gettext en veranderd deze naar een string zodat deze in de toast kan worden gedisplayed
         String input = saldoToevoegen.getText().toString();
@@ -36,11 +37,10 @@ public class AddSaldoActivity extends AppCompatActivity implements View.OnClickL
         cashcounter = new CashCounter();
         cashcounter.Count(getinput);
         int total = cashcounter.getSubtotal();
-        balance.setText(String.format("%.2f", total));
+        saldo.setText(String.format("%.2f", total));
 
 
         Toast.makeText(this, "U heeft " + input + " euro aan uw account toegevoegd", Toast.LENGTH_SHORT).show();
-        balance.setText(input);
 
         //Hier komt de code voor de wallet die je in de title bar kan zien, deze wordt gelijk veranderd zodra je op de knop saldo
         //toevoegen klikt, De limieten van 150 euro en minstens 0 euro komen later.
