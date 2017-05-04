74b1b212e9e4a33c9ada09723842f3c75c465c37
jesse <jwit11@avans.nl>
2017-05-04 10:01:41 +0200

    Probleem opgelost met xml file

--- CustomerApp/app/src/main/java/com/periode4groep2/customerapp/TestActivity.java
+++ CustomerApp/app/src/main/java/com/periode4groep2/customerapp/TestActivity.java
@@ -1,13 +1,16 @@
 package com.periode4groep2.customerapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
-public class TestActivity extends AppCompatActivity {
+public class TestActivity extends AppCompatActivity  {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_test);
+        setContentView(R.layout.activity_test);
     }
 }
