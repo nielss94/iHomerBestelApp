92ed763242e07e73dbce7a47e2f44c8cd661e0f7
rickvoermans <styledbyricky@gmail.com>
2017-05-04 22:58:50 +0200

    Tim zijn user story proberen te fixen
    Pittig veel dingen aangepast

--- CustomerApp/app/src/main/java/com/periode4groep2/customerapp/PresentationLayer/AddSaldoActivity.java
+++ CustomerApp/app/src/main/java/com/periode4groep2/customerapp/PresentationLayer/AddSaldoActivity.java
@@ -5,56 +5,57 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.TextView
 
 import com.periode4groep2.customerapp.R;
 
-import static android.R.attr.value;
 
 public class AddSaldoActivity extends AppCompatActivity implements View.OnClickListener {
     EditText saldoToevoegen;
-    Button stortKnop;
-    EditText firstNumber;
-    EditText secondNumber;
-    TextView addResult;
-
-
-    double num1,num2,sum;
-
+    Button stortKnop, balance;
+    CashCounter cashcounter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_saldo);
         saldoToevoegen = (EditText) findViewById(R.id.addsaldoId);
-        String value = "" + saldoToevoegen.getText();
+
         stortKnop = (Button) findViewById(R.id.stortButton);
         stortKnop.setOnClickListener(this);
-        firstNumber = (EditText)findViewById(R.id.buttonBalance);
-        secondNumber = (EditText)findViewById(R.id.addsaldoId);
-        addResult = (TextView)findViewById(R.id.addsaldoId);
-        stortKnop = (Button)findViewById(R.id.stortButton);
     }
 
     @Override
     public void onClick(View v) {
-        Toast.makeText(this, "Saldo opgewaardeerd met:" +  value, Toast.LENGTH_SHORT).show();
+        balance = (Button)findViewById(R.id.buttonBalance);
+        //Hier laat ik een toast weergeven als de gebruiker op de button heeft geklikt
+        //Hij pakt binnen de onCLick de gettext en veranderd deze naar een string zodat deze in de toast kan worden gedisplayed
+        String input = saldoToevoegen.getText().toString();
+        int getinput = Integer.parseInt(input);
+        cashcounter = new CashCounter();
+        cashcounter.Count(getinput);
+        int total = cashcounter.getSubtotal();
+        balance.setText(String.format("%.2f", total));
+
+
+        Toast.makeText(this, "U heeft " + input + " euro aan uw account toegevoegd", Toast.LENGTH_SHORT).show();
+        balance.setText(input);
+
+        //Hier komt de code voor de wallet die je in de title bar kan zien, deze wordt gelijk veranderd zodra je op de knop saldo
+        //toevoegen klikt, De limieten van 150 euro en minstens 0 euro komen later.
     }
-stortKnop.setOnClickListener(new OnClickListener() {
-        public void onClick(View v) {
-            stortKnop.setOnClickListener(new OnClickListener() {
+}
 
-                public void onClick(View v) {
-                    num1 = Double.parseDouble(firstNumber.getText().toString());
-                    num2 = Double.parseDouble(secondNumber.getText().toString());
-                    sum = num1 + num2;
-                    addResult.setText(Double.toString(sum));
-                }
-            });
-
+    class CashCounter {
+    private int subtotal;
+        public void Count(int price){
+            subtotal += price;
         }
-    });
+
+        public int getSubtotal() {
+            return subtotal;
+        }
 
 }
 
