fc49fbd94d59dd32ccbccd14b0be1752f1b8b856
rickvoermans <styledbyricky@gmail.com>
2017-05-04 10:25:16 +0200

    1 Linear Layout clickable gemaakt (Hoop ik)

--- CustomerApp/app/src/main/java/com/periode4groep2/customerapp/PresentationLayer/HomeScreen.java
+++ CustomerApp/app/src/main/java/com/periode4groep2/customerapp/PresentationLayer/HomeScreen.java
@@ -2,14 +2,25 @@ package com.periode4groep2.customerapp.PresentationLayer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.periode4groep2.customerapp.R;
 
-public class HomeScreen extends AppCompatActivity {
-
+public class HomeScreen extends AppCompatActivity implements View.OnClickListener {
+    LinearLayout orderLayoutButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+
+        orderLayoutButton = (LinearLayout)findViewById(R.id.OrderID);
+        orderLayoutButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(this, "SLETJE", Toast.LENGTH_SHORT).show();
     }
 }
