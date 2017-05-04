b157d035cb8606fc27f33927394151536a6abd50
rickvoermans <styledbyricky@gmail.com>
2017-05-04 14:54:39 +0200

    Kleine aanpassingen
    back button in de toolbar (nog) niet gelukt

--- CustomerApp/app/src/main/java/com/periode4groep2/customerapp/PresentationLayer/MyAccountActivity.java
+++ CustomerApp/app/src/main/java/com/periode4groep2/customerapp/PresentationLayer/MyAccountActivity.java
@@ -2,11 +2,15 @@ package com.periode4groep2.customerapp.PresentationLayer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import com.periode4groep2.customerapp.R;
 
 public class MyAccountActivity extends AppCompatActivity {
-
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
