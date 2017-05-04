d8f517b60cf0cf616a8e9d4f1ee6fc51fefeb699
timsteijn <tjbsteij@avans.nl>
2017-05-04 18:28:14 +0200

    Add saldo knop werkend krijgen

--- CustomerApp/app/src/main/java/com/periode4groep2/customerapp/PresentationLayer/AddSaldoActivity.java
+++ CustomerApp/app/src/main/java/com/periode4groep2/customerapp/PresentationLayer/AddSaldoActivity.java
@@ -5,8 +5,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.TextView
 
 import com.periode4groep2.customerapp.R;
 
@@ -15,6 +15,13 @@ import static android.R.attr.value;
 public class AddSaldoActivity extends AppCompatActivity implements View.OnClickListener {
     EditText saldoToevoegen;
     Button stortKnop;
+    EditText firstNumber;
+    EditText secondNumber;
+    TextView addResult;
+
+
+    double num1,num2,sum;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,11 +31,20 @@ public class AddSaldoActivity extends AppCompatActivity implements View.OnClickL
         String value = "" + saldoToevoegen.getText();
         stortKnop = (Button) findViewById(R.id.stortButton);
         stortKnop.setOnClickListener(this);
+        firstNumber = (EditText)findViewById(R.id.buttonBalance);
+        secondNumber = (EditText)findViewById(R.id.addsaldoId);
+        addResult = (TextView)findViewById(R.id.addsaldoId);
+        stortKnop = (Button)findViewById(R.id.stortButton);
     }
 
     @Override
     public void onClick(View v) {
         Toast.makeText(this, "Saldo opgewaardeerd met:" +  value, Toast.LENGTH_SHORT).show();
+    }
+    public static int addone(int a)
+    {
+        int b = a+1;
+        return b;
     }
 }
 
