a0ff20ced728331f7e06c3c3ea8088e76bd3cf48
rickvoermans <styledbyricky@gmail.com>
2017-05-04 10:35:41 +0200

    Alle buttons clickbaar gemaakt dmv een if else
    met equals, tot nu toe nog ff toast gemaakt omdat de activities nog niet
    zijn gemaakt

--- CustomerApp/app/src/main/java/com/periode4groep2/customerapp/PresentationLayer/HomeScreen.java
+++ CustomerApp/app/src/main/java/com/periode4groep2/customerapp/PresentationLayer/HomeScreen.java
@@ -9,18 +9,33 @@ import android.widget.Toast;
 import com.periode4groep2.customerapp.R;
 
 public class HomeScreen extends AppCompatActivity implements View.OnClickListener {
-    LinearLayout orderLayoutButton;
+    LinearLayout orderLayoutButton, myAcountLayoutButton,
+                  balanceLayoutButton, myOrdersLayoutButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
 
         orderLayoutButton = (LinearLayout)findViewById(R.id.OrderID);
+        myAcountLayoutButton = (LinearLayout)findViewById(R.id.AccountID);
+        balanceLayoutButton = (LinearLayout)findViewById(R.id.BalanceID);
+        myOrdersLayoutButton = (LinearLayout)findViewById(R.id.MyOrdersID);
         orderLayoutButton.setOnClickListener(this);
+        myAcountLayoutButton.setOnClickListener(this);
+        balanceLayoutButton.setOnClickListener(this);
+        myOrdersLayoutButton.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        Toast.makeText(this, "SLETJE", Toast.LENGTH_SHORT).show();
+        if(v.equals(orderLayoutButton)){
+            Toast.makeText(this, "SLETJE ORDERS", Toast.LENGTH_SHORT).show();
+        }else if(v.equals(myAcountLayoutButton)){
+            Toast.makeText(this, "SLETJE ACCOUNT", Toast.LENGTH_SHORT).show();
+        } else if(v.equals(balanceLayoutButton)){
+            Toast.makeText(this, "SLETJE BALANCE", Toast.LENGTH_SHORT).show();
+        } else if (v.equals(myOrdersLayoutButton)){
+            Toast.makeText(this, "SLETJE MY ORDERS", Toast.LENGTH_SHORT).show();
+        }
     }
 }
