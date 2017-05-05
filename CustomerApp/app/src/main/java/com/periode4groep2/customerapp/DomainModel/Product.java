50a0e29e99300203d94e66c1ce96fa82ab360db2
Niels van Dam <nielsvdam@hotmail.com>
2017-05-05 14:06:35 +0200

    Added product class

---
+++ CustomerApp/app/src/main/java/com/periode4groep2/customerapp/DomainModel/Product.java
@@ -0,0 +1,73 @@
+package com.periode4groep2.customerapp.DomainModel;
+
+/**
+ * Created by Niels on 5/5/2017.
+ */
+
+public class Product {
+
+    private int productID;
+    private String category;
+    private String name;
+    private boolean inStock;
+    private Double price;
+
+    public Product(int productID, String category, String name, boolean inStock, Double price) {
+        this.productID = productID;
+        this.category = category;
+        this.name = name;
+        this.inStock = inStock;
+        this.price = price;
+    }
+
+    public int getProductID() {
+        return productID;
+    }
+
+    public void setProductID(int productID) {
+        this.productID = productID;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isInStock() {
+        return inStock;
+    }
+
+    public void setInStock(boolean inStock) {
+        this.inStock = inStock;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "productID=" + productID +
+                ", category='" + category + '\'' +
+                ", name='" + name + '\'' +
+                ", inStock=" + inStock +
+                ", price=" + price +
+                '}';
+    }
+}
