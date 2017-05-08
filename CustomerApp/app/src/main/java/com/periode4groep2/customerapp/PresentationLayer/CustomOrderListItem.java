18bc0e8dd06d90d31db01278f0e8caeaa1c6a00b
Kayvon Rahimi Morad Ali <krahimim@avans.nl>
2017-05-08 13:19:03 +0200

    ListItem voor orderdetail
    nub

---
+++ CustomerApp/app/src/main/java/com/periode4groep2/customerapp/PresentationLayer/CustomOrderListItem.java
@@ -0,0 +1,19 @@
+package com.periode4groep2.customerapp.PresentationLayer;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.periode4groep2.customerapp.R;
+
+/**
+ * Created by Kayvon Rahimi on 8-5-2017.
+ */
+
+public class CustomOrderListItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_list_item_order_history);
+    }
+}
