d9cdb031dbc0e2c3b8b1b42c946707b04234c4ce
Niels van Dam <nielsvdam@hotmail.com>
2017-05-08 13:24:40 +0200

    MySQLAccountDAO now works! Have fun.

--- CustomerApp/app/src/main/java/com/periode4groep2/customerapp/PersistancyLayer/MySQLAccountDAO.java
+++ CustomerApp/app/src/main/java/com/periode4groep2/customerapp/PersistancyLayer/MySQLAccountDAO.java
@@ -1,5 +1,10 @@
 package com.periode4groep2.customerapp.PersistancyLayer;
 
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Handler;
+import android.util.Log;
+
 import com.periode4groep2.customerapp.DomainModel.Account;
 
 import java.util.ArrayList;
@@ -8,14 +13,37 @@ import java.util.ArrayList;
  * Created by Niels on 5/5/2017.
  */
 
-public class MySQLAccountDAO implements AccountDAO {
+public class MySQLAccountDAO implements AccountDAO, MySQLAccountAPIConnector.AccountAvailable {
+
+    private final String TAG = getClass().getSimpleName();
+
+    private ArrayList<Account> accounts = new ArrayList<>();
+    private MySQLAccountAPIConnector mySQLAccountAPIConnector = new MySQLAccountAPIConnector(this);
+    private AccountSetAvailable context;
+
     @Override
-    public ArrayList<Account> selectData() {
-        return null;
+    public ArrayList<Account> selectData(AccountSetAvailable c) {
+        context = c;
+        String[] urls = {
+                "http://10.0.2.2:8080/api/accounts"
+        };
+
+        mySQLAccountAPIConnector.execute(urls);
+
+        return accounts;
     }
 
     @Override
     public void updateData(Account account) {
 
+    }
+
+    @Override
+    public void accountAvailable(Account account, boolean done) {
+        accounts.add(account);
+        if(done)
+        {
+            context.accountSetAvailable(accounts);
+        }
     }
 }
--- CustomerApp/app/src/main/java/com/periode4groep2/customerapp/PresentationLayer/MainActivity.java
+++ CustomerApp/app/src/main/java/com/periode4groep2/customerapp/PresentationLayer/MainActivity.java
@@ -4,20 +4,40 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.periode4groep2.customerapp.DomainModel.Account;
+import com.periode4groep2.customerapp.PersistancyLayer.AccountDAO;
+import com.periode4groep2.customerapp.PersistancyLayer.AccountSetAvailable;
+import com.periode4groep2.customerapp.PersistancyLayer.DAOFactory;
+import com.periode4groep2.customerapp.PersistancyLayer.MySQLAccountDAO;
+import com.periode4groep2.customerapp.PersistancyLayer.MySQLDAOFactory;
 import com.periode4groep2.customerapp.R;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, AccountSetAvailable {
+
+    private final String TAG = getClass().getSimpleName();
     private Button loginButton;
     private Toolbar toolbar;
     private Button balanceButton;
+    private DAOFactory factory;
+    private AccountDAO accountDAO;
+    private ArrayList<Account> accounts = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        factory = new MySQLDAOFactory();
+        accountDAO = factory.createAccountDAO();
+        accountDAO.selectData(this);
 
         loginButton = (Button)findViewById(R.id.inlogKnopId);
         loginButton.setOnClickListener(this);
@@ -27,6 +47,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         balanceButton = (Button) findViewById(R.id.buttonBalance);
         balanceButton.setOnClickListener(this);
+
+
+
     }
 
     //Deze button moet nog veranderd worden wanneer de gegevens kloppen etc
@@ -42,4 +65,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+
+    @Override
+    public void accountSetAvailable(ArrayList<Account> accs) {
+
+        accounts = accs;
+        for (int i = 0; i < accounts.size(); i++) {
+
+            Log.i(TAG,accounts.get(i).toString());
+        }
+    }
 }
