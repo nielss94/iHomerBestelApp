74243b1af9b1c17be45c7e560562f3eb41cae67d
timsteijn <tjbsteij@avans.nl>
2017-05-09 10:50:52 +0200

    Fixx

--- CustomerApp/app/src/main/java/com/periode4groep2/customerapp/PresentationLayer/AddBalanceActivity.java
+++ CustomerApp/app/src/main/java/com/periode4groep2/customerapp/PresentationLayer/AddBalanceActivity.java
@@ -32,33 +32,34 @@ public class AddBalanceActivity extends AppCompatActivity implements View.OnClic
         currentBalance = (TextView) findViewById(R.id.currentBalance);
 
         String input = putInBalance.getText().toString().trim();
-        if(input.isEmpty()){
+        if (input.isEmpty()) {
             Toast.makeText(this, "U heeft geen bedrag ingevoerd", Toast.LENGTH_SHORT).show();
         } else {
             double currBalance = Double.parseDouble(input);
-<<<<<<< HEAD
+
             //Een if else statement om ervoor te zorgen dat er geen bedrag boven de 150 euro komt
             if (currBalance <= 0.01) {
                 Toast.makeText(this, "U kunt niet €0.00 teoevoegen", Toast.LENGTH_SHORT).show();
             } else if (subtotal >= 150.00) {
-=======
-            // Een if else statement om ervoor te zorgen dat er geen bedrag boven de 150 euro komt
-            if (subtotal >= 150.00) {
->>>>>>> origin/master
-                Toast.makeText(this, "U kunt niet meer dan €150,00 op uw account hebben", Toast.LENGTH_SHORT).show();
-            } else if (subtotal + currBalance > 150.00) {
-                Toast.makeText(this, "U kunt niet meer dan €150,00 op uw account hebben", Toast.LENGTH_SHORT).show();
-            } else {
-                Toast.makeText(this, "U heeft " + input + " euro aan uw account toegevoegd", Toast.LENGTH_SHORT).show();
-                subtotal += currBalance;
-                currentBalance.setText(String.format("%.2f", subtotal));
+
+                // Een if else statement om ervoor te zorgen dat er geen bedrag boven de 150 euro komt
+                if (subtotal >= 150.00) {
+
+                    Toast.makeText(this, "U kunt niet meer dan €150,00 op uw account hebben", Toast.LENGTH_SHORT).show();
+                } else if (subtotal + currBalance > 150.00) {
+                    Toast.makeText(this, "U kunt niet meer dan €150,00 op uw account hebben", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "U heeft " + input + " euro aan uw account toegevoegd", Toast.LENGTH_SHORT).show();
+                    subtotal += currBalance;
+                    currentBalance.setText(String.format("%.2f", subtotal));
+                }
             }
-        }
 
-        // EditText leegmaken na gebruik
-        putInBalance.setText("");
+            // EditText leegmaken na gebruik
+            putInBalance.setText("");
 
-        //Hier komt de code voor de wallet die je in de title bar kan zien, deze wordt gelijk veranderd zodra je op de knop saldo
-        //toevoegen klikt, De limieten van 150 euro en minstens 0 euro komen later.
+            //Hier komt de code voor de wallet die je in de title bar kan zien, deze wordt gelijk veranderd zodra je op de knop saldo
+            //toevoegen klikt, De limieten van 150 euro en minstens 0 euro komen later.
         }
     }
+}
