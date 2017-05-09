68547dfcdfe05cbd9b8715a3e6eb1133b3abb578
Kayvon Rahimi Morad Ali <krahimim@avans.nl>
2017-05-09 11:07:12 +0200

    orderItemAdapter
    Alles gecomment.

--- CustomerApp/app/src/main/java/com/periode4groep2/customerapp/PresentationLayer/OrderItemAdapter.java
+++ CustomerApp/app/src/main/java/com/periode4groep2/customerapp/PresentationLayer/OrderItemAdapter.java
@@ -1,46 +1,46 @@
-package com.periode4groep2.customerapp.PresentationLayer;
-
-import android.content.Context;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.TextView;
-
-import com.periode4groep2.customerapp.DomainModel.OrderItem;
-import com.periode4groep2.customerapp.R;
-
-import org.w3c.dom.Text;
-
-import java.util.ArrayList;
-
-/**
- * Created by Kayvon Rahimi on 8-5-2017.
- */
-
-public class OrderItemAdapter extends ArrayAdapter<OrderItem> {
-
-    public OrderItemAdapter(Context context, ArrayList<OrderItem> orderItemList){
-        super(context, R.layout.activity_custom_order_list_item, orderItemList);
-    }
-
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent){
-
-        LayoutInflater productInflater = LayoutInflater.from(getContext());
-        View customView = productInflater.inflate(R.layout.activity_custom_order_list_item, parent, false);
-
-        OrderItem orderItem = getItem(position);
-
-        TextView productName = (TextView)customView.findViewById(R.id.productNameTextView);
-        TextView productQuantity = (TextView)customView.findViewById(R.id.productQuantityTextView);
-        TextView productTotalPrice = (TextView)customView.findViewById(R.id.productTotalPriceTextView);
-
-        productName.setText(orderItem.getProduct());
-        productQuantity.setText(orderItem.getQuantity());
-        productTotalPrice.setText(orderItem.getPrice());
-
-        return customView;
-    }
-
-}
+//package com.periode4groep2.customerapp.PresentationLayer;
+//
+//import android.content.Context;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.ArrayAdapter;
+//import android.widget.TextView;
+//
+//import com.periode4groep2.customerapp.DomainModel.OrderItem;
+//import com.periode4groep2.customerapp.R;
+//
+//import org.w3c.dom.Text;
+//
+//import java.util.ArrayList;
+//
+///**
+// * Created by Kayvon Rahimi on 8-5-2017.
+// */
+//
+//public class OrderItemAdapter extends ArrayAdapter<OrderItem> {
+//
+//    public OrderItemAdapter(Context context, ArrayList<OrderItem> orderItemList){
+//        super(context, R.layout.activity_custom_order_list_item, orderItemList);
+//    }
+//
+//    @Override
+//    public View getView(int position, View convertView, ViewGroup parent){
+//
+//        LayoutInflater productInflater = LayoutInflater.from(getContext());
+//        View customView = productInflater.inflate(R.layout.activity_custom_order_list_item, parent, false);
+//
+//        OrderItem orderItem = getItem(position);
+//
+//        TextView productName = (TextView)customView.findViewById(R.id.productNameTextView);
+//        TextView productQuantity = (TextView)customView.findViewById(R.id.productQuantityTextView);
+//        TextView productTotalPrice = (TextView)customView.findViewById(R.id.productTotalPriceTextView);
+//
+//        productName.setText(orderItem.getProduct());
+//        productQuantity.setText(orderItem.getQuantity());
+//        productTotalPrice.setText(orderItem.getPrice());
+//
+//        return customView;
+//    }
+//
+//}
