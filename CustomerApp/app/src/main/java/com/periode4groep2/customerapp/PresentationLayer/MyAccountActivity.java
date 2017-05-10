c8556dd891ff449da57d5ef3eb5bcb83e2ba71b3
rickvoermans <styledbyricky@gmail.com>
2017-05-10 12:30:44 +0200

    aangepast

--- CustomerApp/app/src/main/java/com/periode4groep2/customerapp/PresentationLayer/MyAccountActivity.java
+++ CustomerApp/app/src/main/java/com/periode4groep2/customerapp/PresentationLayer/MyAccountActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.periode4groep2.customerapp.DomainModel.Account;
 import com.periode4groep2.customerapp.R;
@@ -13,11 +14,13 @@ import com.periode4groep2.customerapp.R;
 public class MyAccountActivity extends AppCompatActivity implements View.OnClickListener{
     Account account;
     private Button balanceInfoButton, orderHistoryButton;
+    private TextView textviewEmail, textviewFirstName, textviewLastName, textviewBirthDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_account);
+
         balanceInfoButton = (Button) findViewById(R.id.myAccountBalanceInfoId);
         orderHistoryButton = (Button) findViewById(R.id.myAccountHistoryId);
 
@@ -25,6 +28,14 @@ public class MyAccountActivity extends AppCompatActivity implements View.OnClick
         balanceInfoButton.setOnClickListener(this);
 
         account = (Account)getIntent().getSerializableExtra("account");
+        textviewEmail = (TextView)findViewById(R.id.my_account_email);
+        textviewEmail.setText(account.getEmail());
+        textviewFirstName = (TextView)findViewById(R.id.my_account_firstname);
+        textviewFirstName.setText(account.getFirstName());
+        textviewLastName = (TextView)findViewById(R.id.my_account_lastname);
+        textviewLastName.setText(account.getLastName());
+        textviewBirthDate = (TextView)findViewById(R.id.my_account_age);
+        textviewBirthDate.setText(account.getDateOfBirth());
     }
 
     @Override
