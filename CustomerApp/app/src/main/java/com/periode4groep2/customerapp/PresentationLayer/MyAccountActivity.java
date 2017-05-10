18792576533d7222b69a41f460320efd9bf34ac2
rickvoermans <styledbyricky@gmail.com>
2017-05-10 19:42:08 +0200

    Age uitrekenen (nog in werking)
    code wordt nog verbeterd en mooier gemaakt (aparte methode etc)

--- CustomerApp/app/src/main/java/com/periode4groep2/customerapp/PresentationLayer/MyAccountActivity.java
+++ CustomerApp/app/src/main/java/com/periode4groep2/customerapp/PresentationLayer/MyAccountActivity.java
@@ -1,7 +1,10 @@
 package com.periode4groep2.customerapp.PresentationLayer;
 
 import android.content.Intent;
+import android.icu.util.Calendar;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -16,6 +19,7 @@ public class MyAccountActivity extends AppCompatActivity implements View.OnClick
     private Button balanceInfoButton, orderHistoryButton;
     private TextView textviewEmail, textviewFirstName, textviewLastName, textviewBirthDate;
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,7 +39,16 @@ public class MyAccountActivity extends AppCompatActivity implements View.OnClick
         textviewLastName = (TextView)findViewById(R.id.my_account_lastname);
         textviewLastName.setText(account.getLastName());
         textviewBirthDate = (TextView)findViewById(R.id.my_account_age);
-        textviewBirthDate.setText(account.getDateOfBirth());
+        //textviewBirthDate.setText(account.getDateOfBirth());
+
+        // FF TESTEN WSS ALLEMAAL BULLSHIT
+        String fullBirthDate = account.getDateOfBirth();
+        String yearsOfBirthDate = fullBirthDate.substring(0, 4);
+        //textviewBirthDate.setText(yearsOfBirthDate);
+        int currentYear = Calendar.getInstance().get(Calendar.YEAR);
+        int birthYear = Integer.parseInt(yearsOfBirthDate);
+        int testAge = currentYear - birthYear;
+        textviewBirthDate.setText(testAge + "");
     }
 
     @Override
