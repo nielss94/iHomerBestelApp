1ecfa1c268a82317e2fba466d434aac8d286542c
jessedewit <wit.jesse.de@gmail.com>
2017-05-14 23:53:44 +0200

    Order history detail activity gemaakt

---
+++ CustomerApp/app/src/main/java/com/periode4groep2/customerapp/PresentationLayer/CustomListItemOrderHistoryDetail.java
@@ -0,0 +1,15 @@
+package com.periode4groep2.customerapp.PresentationLayer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.periode4groep2.customerapp.R;
+
+public class CustomListItemOrderHistoryDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_list_item_order_history_detail);
+    }
+}
