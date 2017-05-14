ad1f5f42e262bbd55609269de1e2bcabbc890c57
jesse <jwit11@avans.nl>
2017-05-14 14:28:04 +0200

    Order history detail

---
+++ CustomerApp/app/src/main/java/com/periode4groep2/customerapp/PresentationLayer/OrderHistoryDetailActivity.java
@@ -0,0 +1,15 @@
+package com.periode4groep2.customerapp.PresentationLayer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.periode4groep2.customerapp.R;
+
+public class OrderHistoryDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_history_detail);
+    }
+}
