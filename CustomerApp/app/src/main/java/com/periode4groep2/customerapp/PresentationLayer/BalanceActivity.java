7fa259f2efab2237584b71d06a0b5b12255e8522
Niels van Dam <nielsvdam@hotmail.com>
2017-05-16 14:01:47 +0200

    Balance wordt nu aangepast op de db

--- CustomerApp/app/src/main/java/com/periode4groep2/customerapp/PresentationLayer/BalanceActivity.java
+++ CustomerApp/app/src/main/java/com/periode4groep2/customerapp/PresentationLayer/BalanceActivity.java
@@ -9,6 +9,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.periode4groep2.customerapp.DomainModel.Account;
+import com.periode4groep2.customerapp.PersistancyLayer.AccountDAO;
+import com.periode4groep2.customerapp.PersistancyLayer.DAOFactory;
+import com.periode4groep2.customerapp.PersistancyLayer.MySQLDAOFactory;
 import com.periode4groep2.customerapp.R;
 
 public class BalanceActivity extends AppCompatActivity implements View.OnClickListener {
@@ -17,11 +20,16 @@ public class BalanceActivity extends AppCompatActivity implements View.OnClickLi
     private Button refundBalance;
     private TextView currentBalanceTextView;
     private Account account;
+    private DAOFactory factory;
+    private AccountDAO accountDAO;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_balance);
+
+        factory = new MySQLDAOFactory();
+        accountDAO = factory.createAccountDAO();
 
         mutateBalance = (EditText) findViewById(R.id.giveBalanceID);
         currentBalanceTextView = (TextView) findViewById(R.id.currentBalance);
@@ -38,8 +46,6 @@ public class BalanceActivity extends AppCompatActivity implements View.OnClickLi
 
     @Override
     public void onClick(View v) {
-
-
         if ( v.equals(addBalance) ) {
 
             double currentBalanceValue = Double.parseDouble(currentBalanceTextView.getText().toString().replaceAll("€", "0"));
@@ -63,6 +69,8 @@ public class BalanceActivity extends AppCompatActivity implements View.OnClickLi
                     Toast.makeText(this, toastText, Toast.LENGTH_SHORT).show();
                     double newBalanceValue = currentBalanceValue + currentEntryValue;
                     currentBalanceTextView.setText("€" + String.format("%.2f", newBalanceValue));
+                    account.setBalance(newBalanceValue);
+                    accountDAO.updateBalance(account,currentEntryValue * 100);
                 }
                 mutateBalance.setText("");
             }
@@ -79,17 +87,15 @@ public class BalanceActivity extends AppCompatActivity implements View.OnClickLi
                 if ( currentEntryValue <= 0.01 ) {
                     Toast.makeText(this, "U kunt niet €0.00 terugstorten op uw rekening.", Toast.LENGTH_SHORT).show();
                 }
-                else if ( currentEntryValue < 5.00 ) {
-                    Toast.makeText(this, "U kunt niet minder dan 5 euro terugstorten.", Toast.LENGTH_SHORT).show();
-                }
                 else if ( currentEntryValue > currentBalanceValue ) {
                     Toast.makeText(this, "U heeft een bedrag ingevuld dat hoger is dan wat er op uw account staat.", Toast.LENGTH_SHORT).show();
                 } else {
                     double newBalanceValue = currentBalanceValue - currentEntryValue;
 
                     Toast.makeText(this, "U heeft " + input + " euro van uw account teruggestort.", Toast.LENGTH_SHORT).show();
-                    currentBalanceTextView.setText(String.format("%.2f", newBalanceValue));
-
+                    currentBalanceTextView.setText("€"+String.format("%.2f", newBalanceValue));
+                    account.setBalance(newBalanceValue);
+                    accountDAO.updateBalance(account,-(currentEntryValue* 100));
                     mutateBalance.setText("");
                 }
             }
