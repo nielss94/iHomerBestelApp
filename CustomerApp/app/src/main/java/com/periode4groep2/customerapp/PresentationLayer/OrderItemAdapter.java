58f66b0e716ec312ff0bd0a65d82243e03a35628
Kayvon Rahimi Morad Ali <krahimim@avans.nl>
2017-05-17 10:21:23 +0200

    Niks noemenswaardig, just a push2pull

--- CustomerApp/app/src/main/java/com/periode4groep2/customerapp/PresentationLayer/OrderItemAdapter.java
+++ CustomerApp/app/src/main/java/com/periode4groep2/customerapp/PresentationLayer/OrderItemAdapter.java
@@ -11,7 +11,9 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+import com.periode4groep2.customerapp.DomainModel.Order;
 import com.periode4groep2.customerapp.DomainModel.OrderItem;
+import com.periode4groep2.customerapp.DomainModel.Product;
 import com.periode4groep2.customerapp.R;
 
 import java.util.ArrayList;
@@ -30,7 +32,8 @@ public class OrderItemAdapter extends ArrayAdapter<OrderItem> {
         View customView = productInflater.inflate(R.layout.activity_custom_order_list_item, parent, false);
 
         OrderItem orderItem = getItem(position);
-        //Order order = new Order();
+        Order order = getItem();
+        Product product = getItem(position);
 
         TextView productName = (TextView)customView.findViewById(R.id.productNameTextView);
         TextView productQuantity = (TextView)customView.findViewById(R.id.productQuantityTextView);
@@ -38,7 +41,7 @@ public class OrderItemAdapter extends ArrayAdapter<OrderItem> {
 
         productName.setText(orderItem.getProductID());
         productQuantity.setText(orderItem.getQuantity());
-        // productTotalPrice.setText(order.getTotalPrice());
+        // productTotalPrice.setText(product.getTotalPrice());
 
         return customView;
     }
