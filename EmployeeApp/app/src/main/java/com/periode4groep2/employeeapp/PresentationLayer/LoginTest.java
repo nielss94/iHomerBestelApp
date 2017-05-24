b3312710cf0a4b52c337bdcf95718bd691af58e0
rickvoermans <styledbyricky@gmail.com>
2017-05-24 12:25:11 +0200

    nfc werkt nu volledig

--- EmployeeApp/app/src/main/java/com/periode4groep2/employeeapp/PresentationLayer/LoginTest.java
+++ EmployeeApp/app/src/main/java/com/periode4groep2/employeeapp/PresentationLayer/LoginTest.java
@@ -1,13 +1,26 @@
 package com.periode4groep2.employeeapp.PresentationLayer;
 
+import android.app.Activity;
+import android.nfc.NfcAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.widget.TextView;
 
+import com.periode4groep2.employeeapp.CardReader.LoyaltyCardReader;
 import com.periode4groep2.employeeapp.R;
 
-public class LoginTest extends AppCompatActivity {
+public class LoginTest extends AppCompatActivity implements LoyaltyCardReader.AccountCallback {
     private Toolbar toolbar;
+    public static final String TAG = "CardReaderFragment";
+    // Recommend NfcAdapter flags for reading from other Android devices. Indicates that this
+    // activity is interested in NFC-A devices (including other Android devices), and that the
+    // system should not check for the presence of NDEF-formatted data (e.g. Android Beam).
+    public static int READER_FLAGS =
+            NfcAdapter.FLAG_READER_NFC_A | NfcAdapter.FLAG_READER_SKIP_NDEF_CHECK;
+    public LoyaltyCardReader mLoyaltyCardReader;
+    private TextView mAccountField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +30,55 @@ public class LoginTest extends AppCompatActivity {
         toolbar = (Toolbar) findViewById(R.id.tool_bar_no_button);
         setSupportActionBar(toolbar);
 
+        mAccountField = (TextView) findViewById(R.id.test);
+        mAccountField.setText("Waiting...");
+
+        mLoyaltyCardReader = new LoyaltyCardReader(this);
+
+        // Disable Android Beam and register our card reader callback
+        //enableReaderMode();
+
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        disableReaderMode();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        enableReaderMode();
+    }
+
+    private void enableReaderMode() {
+        Log.i(TAG, "Enabling reader mode");
+        Activity activity = this;
+        NfcAdapter nfc = NfcAdapter.getDefaultAdapter(activity);
+        if (nfc != null) {
+            nfc.enableReaderMode(activity, mLoyaltyCardReader, READER_FLAGS, null);
+        }
+    }
+
+    private void disableReaderMode() {
+        Log.i(TAG, "Disabling reader mode");
+        Activity activity = this;
+        NfcAdapter nfc = NfcAdapter.getDefaultAdapter(activity);
+        if (nfc != null) {
+            nfc.disableReaderMode(activity);
+        }
+    }
+
+    @Override
+    public void onAccountReceived(final String account) {
+        // This callback is run on a background thread, but updates to UI elements must be performed
+        // on the UI thread.
+        this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mAccountField.setText(account);
+            }
+        });
     }
 }
