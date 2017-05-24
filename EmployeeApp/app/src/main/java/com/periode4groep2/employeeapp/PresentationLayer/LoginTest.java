a2089d48a9f99d33d43fbc6226ca2babb6d6fcb4
Kayvon Rahimi Morad Ali <krahimim@avans.nl>
2017-05-24 10:11:46 +0200

    Receive intent in loginTest
    a

--- EmployeeApp/app/src/main/java/com/periode4groep2/employeeapp/PresentationLayer/LoginTest.java
+++ EmployeeApp/app/src/main/java/com/periode4groep2/employeeapp/PresentationLayer/LoginTest.java
@@ -3,13 +3,16 @@ package com.periode4groep2.employeeapp.PresentationLayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.periode4groep2.employeeapp.DomainModel.Account;
 import com.periode4groep2.employeeapp.R;
 
 public class LoginTest extends AppCompatActivity {
-
+    private Account account;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_test);
+
+        account = (Account)getIntent().getSerializableExtra("account");
     }
 }
