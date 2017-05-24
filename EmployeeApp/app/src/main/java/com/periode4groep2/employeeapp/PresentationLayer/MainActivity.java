49a797c4ed8ca73d08303900d9743e08c5008c9e
JD <jmcwater@avans.student.nl>
2017-05-24 09:47:52 +0200

    kleinklein

--- EmployeeApp/app/src/main/java/com/periode4groep2/employeeapp/PresentationLayer/MainActivity.java
+++ EmployeeApp/app/src/main/java/com/periode4groep2/employeeapp/PresentationLayer/MainActivity.java
@@ -19,7 +19,7 @@ import com.periode4groep2.employeeapp.PersistancyLayer.MySQLDAOFactory;
 import com.periode4groep2.employeeapp.R;
 
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener,MyAccountAvailable {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, MyAccountAvailable {
     private Button loginButton;
     private Toolbar toolbar;
     private DAOFactory factory;
@@ -27,11 +27,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private EditText email, password;
     private Account account;
 
+
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+        editor = sharedPreferences.edit();
         factory = new MySQLDAOFactory();
         accountDAO = factory.createAccountDAO();
 
@@ -41,14 +47,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         email = (EditText)findViewById(R.id.emailInputId);
         password = (EditText)findViewById(R.id.passwordInputId);
 
-        toolbar = (Toolbar) findViewById(R.id.tool_bar_no_button);
-        setSupportActionBar(toolbar);
+//        toolbar = (Toolbar) findViewById(R.id.tool_bar_no_button);
+//        setSupportActionBar(toolbar);
+
+       // editor.clear();
+
     }
 
     @Override
     public void onClick(View v) {
         //Check if email/password is empty
-        accountDAO.selectData(this ,email.getText().toString(),password.getText().toString());
+        accountDAO.selectData(this,email.getText().toString(),password.getText().toString());
     }
 
     @Override
@@ -63,8 +72,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     public void logIn(){
+
+        editor.putString("Email", account.getEmail());
+        editor.commit();
+        Log.i("", sharedPreferences.getString("Email","emaliltjw"));
         Intent intent = new Intent(this, LoginTest.class);
+        intent.putExtra("account", account);
         startActivity(intent);
         Toast.makeText(this, R.string.successful_log_in_toast, Toast.LENGTH_SHORT).show();
+
+
     }
 }
