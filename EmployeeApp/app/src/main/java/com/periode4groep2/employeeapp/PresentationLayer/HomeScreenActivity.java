c176fafa2bd756d88c23cfa14d75420e19d134f2
Kayvon Rahimi Morad Ali <krahimim@avans.nl>
2017-05-31 09:24:02 +0200

    Added intents for HomeScreen.
    Commented until target activities are added.

--- EmployeeApp/app/src/main/java/com/periode4groep2/employeeapp/PresentationLayer/HomeScreenActivity.java
+++ EmployeeApp/app/src/main/java/com/periode4groep2/employeeapp/PresentationLayer/HomeScreenActivity.java
@@ -38,9 +38,12 @@ public class HomeScreenActivity extends AppCompatActivity implements View.OnClic
             i.putExtra("account", account);
             startActivity(i);
         }else if(v.equals(searchOrdersView)){
-
+//            Intent i = new Intent(this, SearchOrderActivity.class);
+//            i.putExtra("account", account);
+//            startActivity(i);
         }else if(v.equals(stockView)){
-
+//            Intent i = new Intent(this, StockActivity.class);
+//            i.putExtra("account", account);
         }
     }
 }
