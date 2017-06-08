40270c3c9058e6577b3c48c741fc378f4ec72e22
rickvoermans <styledbyricky@gmail.com>
2017-06-08 12:11:48 +0200

    Buttons laten inspawnen per product

--- EmployeeApp/app/src/main/java/com/periode4groep2/employeeapp/PresentationLayer/AddExtraProducts.java
+++ EmployeeApp/app/src/main/java/com/periode4groep2/employeeapp/PresentationLayer/AddExtraProducts.java
@@ -2,10 +2,16 @@ package com.periode4groep2.employeeapp.PresentationLayer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 
 import com.periode4groep2.employeeapp.R;
 
+import java.util.ArrayList;
+
 public class AddExtraProducts extends AppCompatActivity {
+    private ArrayList<ProductButton> productButtons = new ArrayList<>();
+    private LinearLayout layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
