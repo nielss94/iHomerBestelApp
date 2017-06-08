99904cbe0cc62c9cef5f6c3a191f7fb73c3e44ce
jordanh2os <jmc.waters@student.avans.nl>
2017-06-08 12:09:19 +0200

    adsd

--- EmployeeApp/app/src/main/java/com/periode4groep2/employeeapp/PresentationLayer/AddExtraProducts.java
+++ EmployeeApp/app/src/main/java/com/periode4groep2/employeeapp/PresentationLayer/AddExtraProducts.java
@@ -11,5 +11,7 @@ public class AddExtraProducts extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_products_activity);
+
+        //mic-check-1-2
     }
 }
