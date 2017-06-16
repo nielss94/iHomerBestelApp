00c5a8575886928203f6f48952fc823e121fa38e
Lars Smans <mhsmans@avans.nl>
2017-06-16 12:56:01 +0200

    Werkt nog niet , maar het is wat meer

--- EmployeeApp/app/src/main/java/com/periode4groep2/employeeapp/PresentationLayer/AddStockProduct.java
+++ EmployeeApp/app/src/main/java/com/periode4groep2/employeeapp/PresentationLayer/AddStockProduct.java
@@ -7,16 +7,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.periode4groep2.employeeapp.DomainModel.Account;
+import com.periode4groep2.employeeapp.DomainModel.Product;
+import com.periode4groep2.employeeapp.PersistancyLayer.MySQLProductDAO;
 import com.periode4groep2.employeeapp.R;
 
-public class AddStockProduct extends AppCompatActivity {
+public class AddStockProduct extends AppCompatActivity implements View.OnClickListener{
 
     private Toolbar toolbar;
     private Account account;
-    private EditText productID, category, name, price, inStock, nameEng, categoryEng;
+    private EditText category, name, price, nameEng, categoryEng;
+    private Product product;
+    private Button button;
+    private MySQLProductDAO productDAO;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,13 +31,14 @@ public class AddStockProduct extends AppCompatActivity {
 
         account = (Account) getIntent().getSerializableExtra("account");
 
-        productID = (EditText) findViewById(R.id.stock_add_product_productID);
         category = (EditText) findViewById(R.id.stock_add_product_category);
         name = (EditText) findViewById(R.id.stock_add_product_product);
         price = (EditText) findViewById(R.id.stock_add_product_price);
-        inStock = (EditText) findViewById(R.id.stock_add_product_instock);
         nameEng = (EditText) findViewById(R.id.stock_add_product_nameEng);
         categoryEng = (EditText) findViewById(R.id.stock_add_product_categoryEng);
+
+        button = (Button) findViewById(R.id.stock_add_product_button);
+        button.setOnClickListener(this);
 
         toolbar = (Toolbar) findViewById(R.id.tool_bar_no_button);
         Drawable homeButton = ContextCompat.getDrawable(getApplicationContext(), R.drawable.ic_homebutton);
@@ -48,4 +55,19 @@ public class AddStockProduct extends AppCompatActivity {
             }
         });
     }
+
+    public void onClick(View v){
+        product = new Product(0, category.getText().toString(), name.getText().toString(), true,
+                Double.parseDouble(price.getText().toString()));
+        product.setNameEng(nameEng.getText().toString());
+        product.setCategoryEng(categoryEng.getText().toString());
+
+        productDAO.addProductToStock(account, product);
+
+        Intent intent = new Intent(getApplicationContext(), HomeScreenActivity.class);
+        intent.putExtra("account", account);
+        startActivity(intent);
+    }
+
+
 }
